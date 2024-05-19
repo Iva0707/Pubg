0d3b859a9a1392e4892cd14ca3c7bf5b722ff6a8
Iva Tkachuk <ivatkachuk@MacBook---Iva.local>
2024-05-19 16:10:57 +0200

    first

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -3,3 +3,9 @@ Fancybox.bind("[data-fancybox]", {});
 
 // _____slick_____
 $('.single-item').slick();
+
+
+// _____niceSelect_____
+$(document).ready(function() {
+    $('select').niceSelect();
+});
