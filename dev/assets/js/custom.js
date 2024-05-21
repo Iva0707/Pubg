be364689b93b5f75f47d01be30a5430fcef8eb2e
Iva Tkachuk <ivatkachuk@MacBook---Iva.local>
2024-05-21 22:32:16 +0200

    first screen

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
     $('select').niceSelect();
 });
 
+// _____burger_____
 const burger = document.querySelector('#burger');
 const navSection = document.querySelector('.nav_list');
 
@@ -19,3 +20,20 @@ function showNav(){
 }
 
 burger.addEventListener('click', showNav);
+
+
+// _____nav_menu_____
+const mediaQuery = window.matchMedia("(min-width: 991px)")
+function myFunction(mediaQuery) {
+  if (mediaQuery.matches) { 
+    navSection.removeAttribute('style')
+  } else {
+    navSection.style.transition = '0.3s';
+  }
+}
+
+myFunction(mediaQuery);
+
+mediaQuery.addEventListener("change", function() {
+  myFunction(mediaQuery);
+});
